9b8ec8d3a601ae66f99cdd36b95c3335a1b43a36
Barba <pippobarba02@gmail.com>
2023-08-09 16:52:17 +0200

    first commit

---
+++ src/main/java/com/filippoBarbieri/gestionePassaporti/GestionePassaportiApplication.java
@@ -0,0 +1,13 @@
+package com.filippoBarbieri.gestionePassaporti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePassaportiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePassaportiApplication.class, args);
+	}
+
+}
