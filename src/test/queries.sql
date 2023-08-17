ab2d138b2accc3776713c32acf3bee427307a98c
Barba <pippobarba02@gmail.com>
2023-08-17 17:23:05 +0200

    test cittadino controller

---
+++ src/test/queries.sql
@@ -0,0 +1,9 @@
+insert into anagrafica values ('2002-10-01', 'BRBFPP02R01E349J', 'Barbieri', 'Isola della Scala', 'Italiana', 'Filippo');
+insert into cittadino values ('2002-10-01', false, false, false, 'BRBFPP02R01E349J', 'Barbieri', 'Isola della Scala', 'Italiana', 'Filippo', 'psw', 'ts');
+insert into slot values ('2021-12-01 14:30:15', 'QUESTURA_VERONA', 'LIBERO', 'RILASCIO');
+insert into prenotazione values ('2021-12-01 14:30:15', 'BRBFPP02R01E349J');
+
+select * from anagrafica;
+select * from cittadino;
+select * from slot;
+select * from prenotazione;
