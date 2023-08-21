d8e882d24dff35c6fb7702f66c5ff7ca00f13e96
Barba <pippobarba02@gmail.com>
2023-08-21 16:18:31 +0200

    test inserimento anagrafica db

--- src/test/java/com/filippoBarbieri/gestionePassaporti/GestionePassaportiApplicationTests.java
+++ src/test/java/com/filippoBarbieri/gestionePassaporti/GestionePassaportiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.filippoBarbieri.gestionePassaporti;
 
+
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.filippoBarbieri.gestionePassaporti.entity.Anagrafica;
+import com.filippoBarbieri.gestionePassaporti.repository.AnagraficaRepository;
 
 @SpringBootTest
 class GestionePassaportiApplicationTests {
+	@Autowired
+	AnagraficaRepository anagraficaRepo;
 
 	@Test
 	void contextLoads() {
+		String[] cfValidi = {"BRBFPP02R01E349J", "MRRCSR02C30E512F", "CNTDRD99M06A465H", "VRTNRC99T15E512P"};
+		Arrays.stream(cfValidi).parallel().map(Anagrafica::new).forEach(cf -> anagraficaRepo.save(cf));
 	}
-
 }
