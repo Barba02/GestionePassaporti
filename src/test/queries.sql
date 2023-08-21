87af9b52282deef55e7ef66bfa96f6a3b2792822
Barba <pippobarba02@gmail.com>
2023-08-21 16:30:27 +0200

    query restyling

--- src/test/queries.sql
+++ src/test/queries.sql
@@ -1,4 +1,4 @@
-insert into anagrafica values ('2002-10-01', 'BRBFPP02R01E349J', 'Barbieri', 'Isola della Scala', 'Italiana', 'Filippo');
+insert into anagrafica values ('BRBFPP02R01E349J');
 insert into cittadino values ('2002-10-01', false, false, false, 'BRBFPP02R01E349J', 'Barbieri', 'Isola della Scala', 'Italiana', 'Filippo', 'psw', 'ts');
 insert into slot values ('2021-12-01 14:30:15', 'QUESTURA_VERONA', 'LIBERO', 'RILASCIO');
 insert into prenotazione values ('2021-12-01 14:30:15', 'BRBFPP02R01E349J');
