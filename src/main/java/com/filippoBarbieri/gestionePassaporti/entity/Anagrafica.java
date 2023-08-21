23b03677c8a7ded3e58be41ff57e834076f9d5ba
Barba <pippobarba02@gmail.com>
2023-08-21 16:17:26 +0200

    restyling architetturale

--- src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Anagrafica.java
+++ src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Anagrafica.java
@@ -1,28 +1,19 @@
 package com.filippoBarbieri.gestionePassaporti.entity;
 
 
-import java.sql.Date;
 import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class Anagrafica {
     @Id
-    protected String cf;
-    @NotNull
-    protected String nome;
-    @NotNull
-    protected String cognome;
-    @NotNull
-    protected String nazionalita;
-    @NotNull
-    protected Date data_nascita;
-    @NotNull
-    protected String luogo_nascita;
+    private String cf;
+
+    public Anagrafica() {}
+
+    public Anagrafica(String cf) {
+        this.cf = cf;
+    }
 
     public String getCf() {
         return cf;
@@ -30,45 +21,5 @@ public class Anagrafica {
 
     public void setCf(String cf) {
         this.cf = cf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getNazionalita() {
-        return nazionalita;
-    }
-
-    public void setNazionalita(String nazionalita) {
-        this.nazionalita = nazionalita;
-    }
-
-    public Date getData_nascita() {
-        return data_nascita;
-    }
-
-    public void setData_nascita(Date data_nascita) {
-        this.data_nascita = data_nascita;
-    }
-
-    public String getLuogo_nascita() {
-        return luogo_nascita;
-    }
-
-    public void setLuogo_nascita(String luogo_nascita) {
-        this.luogo_nascita = luogo_nascita;
     }
 }
--- src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Cittadino.java
+++ src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Cittadino.java
@@ -1,12 +1,26 @@
 package com.filippoBarbieri.gestionePassaporti.entity;
 
 
+import java.sql.Date;
+import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import org.apache.commons.codec.digest.DigestUtils;
 
 @Entity
-public class Cittadino extends Anagrafica {
+public class Cittadino {
+    @Id
+    private String cf;
+    @NotNull
+    private String nome;
+    @NotNull
+    private String cognome;
+    @NotNull
+    private String nazionalita;
+    @NotNull
+    private Date data_nascita;
+    @NotNull
+    private String luogo_nascita;
     @NotNull
     private String ts;
     @NotNull
@@ -18,12 +32,68 @@ public class Cittadino extends Anagrafica {
     @NotNull
     private Boolean di_servizio;
 
+    public String getCf() {
+        return cf;
+    }
+
+    public void setCf(String cf) {
+        this.cf = cf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNazionalita() {
+        return nazionalita;
+    }
+
+    public void setNazionalita(String nazionalita) {
+        this.nazionalita = nazionalita;
+    }
+
+    public Date getData_nascita() {
+        return data_nascita;
+    }
+
+    public void setData_nascita(Date data_nascita) {
+        this.data_nascita = data_nascita;
+    }
+
+    public String getLuogo_nascita() {
+        return luogo_nascita;
+    }
+
+    public void setLuogo_nascita(String luogo_nascita) {
+        this.luogo_nascita = luogo_nascita;
+    }
+
     public String getTs() {
         return ts;
     }
 
     public void setTs(String ts) {
         this.ts = ts;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = DigestUtils.sha256Hex(password);
     }
 
     public Boolean getFigli_minori() {
@@ -48,13 +118,5 @@ public class Cittadino extends Anagrafica {
 
     public void setDi_servizio(Boolean di_servizio) {
         this.di_servizio = di_servizio;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = DigestUtils.sha256Hex(password);
     }
 }
