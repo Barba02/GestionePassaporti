a391f7e56f654cf01e36c6fe62eca878d889cb4a
Barba <pippobarba02@gmail.com>
2023-08-28 19:47:53 +0200

    eccezioni api

--- src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Anagrafica.java
+++ src/main/java/com/filippoBarbieri/gestionePassaporti/entity/Anagrafica.java
@@ -1,22 +1,21 @@
 package com.filippoBarbieri.gestionePassaporti.entity;
 
 
-import java.io.IOException;
 import java.net.URI;
+import java.sql.Date;
+import java.util.Calendar;
+import org.json.JSONObject;
+import java.io.IOException;
 import java.net.URLEncoder;
+import jakarta.persistence.Id;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.sql.Date;
-import java.util.Calendar;
-
-import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Transient;
+import java.nio.charset.StandardCharsets;
 import jakarta.validation.constraints.NotNull;
-import org.json.JSONObject;
 
 @Entity
 public class Anagrafica {
@@ -52,30 +51,30 @@ public class Anagrafica {
         this.nato_maschio = nato_maschio;
         this.provincia_nascita = provincia_nascita;
         CAL.setTime(data_nascita);
-        cf = retrieveCf();
+        try {
+            cf = retrieveCf();
+        }
+        catch (IOException | InterruptedException e) {
+            cf = "";
+        }
     }
 
-    private String retrieveCf() {
-        try {
-            String apiUrlWithParams = "https://api.miocodicefiscale.com/calculate" +
-                    "?lname=" + URLEncoder.encode(cognome, StandardCharsets.UTF_8) +
-                    "&fname=" + URLEncoder.encode(nome, StandardCharsets.UTF_8) +
-                    "&gender=" + URLEncoder.encode((nato_maschio) ? "M" : "F", StandardCharsets.UTF_8) +
-                    "&city=" + URLEncoder.encode(luogo_nascita, StandardCharsets.UTF_8) +
-                    "&state=" + URLEncoder.encode(provincia_nascita, StandardCharsets.UTF_8) +
-                    "&day=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.DAY_OF_MONTH)), StandardCharsets.UTF_8) +
-                    "&month=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.MONTH)+1), StandardCharsets.UTF_8) +
-                    "&year=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.YEAR)), StandardCharsets.UTF_8) +
-                    "&omocodia_level=1&access_token=b8b3b094feb0088aa1d612e742690f1d3816e8a728ad034193a9c6c5c6a3e64d49e";
-            HttpRequest req = HttpRequest.newBuilder().uri(URI.create(apiUrlWithParams)).GET().build();
-            HttpResponse<String> response = HttpClient.newHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 200)
-                return "";
-            return new JSONObject(response.body()).getJSONObject("data").getString("cf");
-        }
-        catch (InterruptedException | IOException e) {
-            return "";
-        }
+    private String retrieveCf() throws IOException, InterruptedException {
+        String apiUrlWithParams = "https://api.miocodicefiscale.com/calculate" +
+                "?lname=" + URLEncoder.encode(cognome, StandardCharsets.UTF_8) +
+                "&fname=" + URLEncoder.encode(nome, StandardCharsets.UTF_8) +
+                "&gender=" + URLEncoder.encode((nato_maschio) ? "M" : "F", StandardCharsets.UTF_8) +
+                "&city=" + URLEncoder.encode(luogo_nascita, StandardCharsets.UTF_8) +
+                "&state=" + URLEncoder.encode(provincia_nascita, StandardCharsets.UTF_8) +
+                "&day=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.DAY_OF_MONTH)), StandardCharsets.UTF_8) +
+                "&month=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.MONTH)+1), StandardCharsets.UTF_8) +
+                "&year=" + URLEncoder.encode(String.valueOf(CAL.get(Calendar.YEAR)), StandardCharsets.UTF_8) +
+                "&omocodia_level=1&access_token=b8b3b094feb0088aa1d612e742690f1d3816e8a728ad034193a9c6c5c6a3e64d49e";
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(apiUrlWithParams)).GET().build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200)
+            throw new InterruptedException("Response code not valid");
+        return new JSONObject(response.body()).getJSONObject("data").getString("cf");
     }
 
     public String getCf() {
