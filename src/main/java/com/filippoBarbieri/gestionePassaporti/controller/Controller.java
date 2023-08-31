95e3d9887c15850d6d6af0a25079507be45cc983
Barba <pippobarba02@gmail.com>
2023-08-31 11:08:49 +0200

    handler errore generico

--- src/main/java/com/filippoBarbieri/gestionePassaporti/controller/Controller.java
+++ src/main/java/com/filippoBarbieri/gestionePassaporti/controller/Controller.java
@@ -7,9 +7,17 @@ import com.filippoBarbieri.gestionePassaporti.dto.ErroreDTO;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public abstract class Controller {
+    static class ErroreNonGestitoDTO extends ErroreDTO {
+        public ErroreNonGestitoDTO(String tipo, String messaggio, String path) {
+            super(tipo, messaggio);
+        }
+    }
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErroreDTO> gestioneErroreCentralizzata(Exception e) {
-        ErroreDTO dto = new ErroreDTO(e.getClass().getSimpleName(), e.getMessage());
+        StackTraceElement ste = e.getStackTrace()[0];
+        ErroreDTO dto = new ErroreNonGestitoDTO(e.getClass().getSimpleName(),
+                e.getMessage(),
+                ste.getClassName() + "." + ste.getMethodName());
         return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/test/queries.sql
+++
@@ -1,7 +0,0 @@
-insert into slot(sede, datetime, stato, tipo) values ('QUESTURA_VERONA', '2021-12-01 14:30:15', 'LIBERO', 'RILASCIO');
-insert into cittadino(anagrafica_cf, password, cie, figli_minori, diplomatico) values ('BRBFPP02R01E349J', 'P@ssw0rd', 'CA68583IM', false, false);
-insert into anagrafica(cf, nome, cognome, data_nascita, luogo_nascita, nazionalita) values ('BRBFPP02R01E349J', 'Filippo', 'Barbieri', '2002-10-01', 'Isola della Scala', 'Italiana');
-
-select * from slot;
-select * from cittadino;
-select * from anagrafica;
