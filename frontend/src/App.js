09f481c57509f1eafc57c8ac4bd56927b851eb65
Barba <pippobarba02@gmail.com>
2023-09-01 19:51:02 +0200

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+function Bollino({ link, icona, descrizione }) {
+    function handleClick() {
+        console.log(link);
+    }
+    return (
+        <div className="bollinoLogin">
+            <i className={icona} onClick={handleClick}></i>
+            <p dangerouslySetInnerHTML={{ __html: descrizione }} onClick={handleClick}></p>
+        </div>
+    );
+}
+
 function App() {
-  return <h1>Hello World</h1>
+    return (
+        <div className="riquadroLogin">
+          <Bollino link="login_cittadino" icona="fa-solid fa-user" descrizione="Login<br/>Cittadino" />
+          <Bollino link="registrazione_cittadino" icona="fa-solid fa-user-plus" descrizione="Registrazione<br/>Cittadino" />
+          <Bollino link="login_dipendente" icona="fa-solid fa-user-tie" descrizione="Login<br/>Dipendente" />
+        </div>
+    );
 }
 
 export default App;
